64b0d2ac77fd4314ffca5ff69d3da5f1e43ae5da
Dhanwanthari Ramakrishnan <dan13ram@gmail.com>
2020-06-04 12:39:26 +0530

    changed about content

--- src/components/pages/aboutComponent/aboutPage.js
+++ src/components/pages/aboutComponent/aboutPage.js
@@ -20,19 +20,10 @@ class AboutPage extends React.Component {
                 <div className="about-page" >
                     <div id="about-photo">
                         <div id="about-img-cropper" >
-                            <img src={photo} id="about-img" alt="profile" />
+                            {/* <img src={photo} id="about-img" alt="profile" /> */}
                         </div>
                     </div>
                     <div id="about-content">
-                        <p> Hello!! </p>
-                        <p> I'm Varsha</p>
-                        <p> I'm the cutest of the cutes</p>
-                        <p> I enjoy drawing </p>
-                        <p> I enjoy dancing </p>
-                        <p> I enjoy tea </p>
-                        <p> I also make dumb noises and tell people that random birds made them </p>
-                        <p> You can contact me at the below email </p>
-                        <p> varsha94[dot]n [at] gmail [dot] com</p>
                     </div>
                 </div>
                  
